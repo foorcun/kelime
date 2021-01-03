e84266460cda6fc9d4ab4bd0ed5c951ed368afdf
User <User@192.168.1.5>
2021-01-03 21:34:24 +0200

    repo die oluştruduğum gereksiz package silip DataAccess oluşuturduk

--- kelime/src/main/java/com/troyatech/kelime/DataAccess/JsonKelimeDal.java
+++ kelime/src/main/java/com/troyatech/kelime/DataAccess/JsonKelimeDal.java
@@ -1,4 +1,4 @@
-package com.troyatech.kelime.repository;
+package com.troyatech.kelime.DataAccess;
 
 import java.io.FileNotFoundException;
 import java.io.FileReader;
@@ -16,67 +16,76 @@ import org.springframework.stereotype.Repository;
 import com.troyatech.kelime.model.Kelime;
 
 @Repository
-public class JsonKelimeRepositoryDal implements IKelimeDal{
+public class JsonKelimeDal implements IKelimeDal {
+	
+	
+	
+//	IKelimeRepo jsonKelimeRepo = new JsonKelimeRepository();
+//	IKelimeRepo jsonKelimeRepo;
 
-	
-	
+//	@Autowired
+//	public JsonKelimeDal(IKelimeRepo jsonKelimeRepo) {
+//		this.jsonKelimeRepo = jsonKelimeRepo	;
+//	}
 	
 	@Autowired
-	public JsonKelimeRepositoryDal() {
-		// TODO Auto-generated constructor stub
+	public JsonKelimeDal( ) {
 	}
+	
+	
+	
 	
 
 	@Override
-	public List<Kelime> findAll() throws IOException, ParseException {
+	public List<Kelime> getAll() throws FileNotFoundException, IOException, ParseException {
 		// TODO Auto-generated method stub
-		//link: https://www.youtube.com/watch?v=_vdiWUDbruw&ab_channel=BrandanJones
-		
-		// kelime list
-		List<Kelime> kelimes = new ArrayList<Kelime>();
+				//link: https://www.youtube.com/watch?v=_vdiWUDbruw&ab_channel=BrandanJones
+				
+				// kelime list
+				List<Kelime> kelimes = new ArrayList<Kelime>();
 
-		//raw json
-		//FileReader reader = new FileReader(".\\jsonFiles\\JsonKelimeler.json");
-		FileReader reader = new FileReader(".\\jsonFiles\\kelimelerim.json");
-		//json array
-		JSONParser jsonparser = new JSONParser();
-		Object obj = jsonparser.parse(reader);
-		JSONArray keljsonobjArr = (JSONArray)obj;
-		//for
-		
-		for(int i = 0; i<  keljsonobjArr.size(); i++) {
-		
-		//jsonObject
-		JSONObject jsonKelime =  (JSONObject) keljsonobjArr.get(i);
-		// klasımız
-		Kelime kelime = new Kelime();		
-		//data get id,.....
-		String id = (String) jsonKelime.get("id");
-		String ger = (String) jsonKelime.get("ger");
-		String eng = (String) jsonKelime.get("eng");
-		String tr = (String) jsonKelime.get("tr");
-		String acıklama = (String) jsonKelime.get("acıklama");
-		String puan = (String) jsonKelime.get("puan");
-		
-		//data set id,....
-		kelime.setId(id);
-		kelime.setGer(ger);
-		kelime.setEng(eng);
-		kelime.setTr(tr);
-		kelime.setAcıklama(acıklama);
-		kelime.setPuan(puan);
-		
-		
-		
-		//kelime listesine add
-		kelimes.add(kelime);
-		}
-		
-		return kelimes;
+				//raw json
+				//FileReader reader = new FileReader(".\\jsonFiles\\JsonKelimeler.json");
+				FileReader reader = new FileReader(".\\jsonFiles\\kelimelerim.json");
+				//json array
+				JSONParser jsonparser = new JSONParser();
+				Object obj = jsonparser.parse(reader);
+				JSONArray keljsonobjArr = (JSONArray)obj;
+				//for
+				
+				for(int i = 0; i<  keljsonobjArr.size(); i++) {
+				
+				//jsonObject
+				JSONObject jsonKelime =  (JSONObject) keljsonobjArr.get(i);
+				// klasımız
+				Kelime kelime = new Kelime();		
+				//data get id,.....
+				String id = (String) jsonKelime.get("id");
+				String ger = (String) jsonKelime.get("ger");
+				String eng = (String) jsonKelime.get("eng");
+				String tr = (String) jsonKelime.get("tr");
+				String acıklama = (String) jsonKelime.get("acıklama");
+				String puan = (String) jsonKelime.get("puan");
+				
+				//data set id,....
+				kelime.setId(id);
+				kelime.setGer(ger);
+				kelime.setEng(eng);
+				kelime.setTr(tr);
+				kelime.setAcıklama(acıklama);
+				kelime.setPuan(puan);
+				
+				
+				
+				//kelime listesine add
+				kelimes.add(kelime);
+				}
+				
+				return kelimes;
 	}
+
 	
-
-
+	
 	@Override
 	public Kelime getById(String id) throws IOException, ParseException {
 		// TODO Auto-generated method stub
@@ -120,26 +129,23 @@ public class JsonKelimeRepositoryDal implements IKelimeDal{
 //		return null;
 	}
 
+
 	@Override
-	public void Add(Kelime kelime) {
+	public void add(Kelime kelime) {
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
-	public void Update(int id, Kelime kelime) {
+	public void update(Kelime kelime) {
 		// TODO Auto-generated method stub
 		
 	}
 
 	@Override
-	public void Delete(int id) {
+	public void delete(Kelime kelime) {
 		// TODO Auto-generated method stub
 		
 	}
-
-
-
-
 
 }
